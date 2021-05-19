54b6da37fa68b7658de1d4c84383978e5346a770
kkala <kkala27@gmail.com>
2021-05-19 21:02:19 +0530

    Swagger Configuations added

---
+++ src/main/java/com/harman/um/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.harman.um.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.models.Contact;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.harman.um.controller"))
+                .paths(regex("/user-management.*"))
+                .build();
+                
+    }
+
+	
+}
--- src/main/java/com/harman/um/controller/UserDetailsController.java
+++ src/main/java/com/harman/um/controller/UserDetailsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import com.harman.um.model.UserDetails;
 import com.harman.um.service.UserDetailsService;
 
 @RestController
+@RequestMapping("/user-management")
 public class UserDetailsController {
 
 	@Autowired
